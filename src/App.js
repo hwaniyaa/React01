4bd2bb5690b6d8f8143e5908a2ac7a0335a5296f
hwani <ghksl12q@gmail.com>
2023-03-19 21:43:45 +0900

    react

---
+++ src/App.js
@@ -0,0 +1,76 @@
+import { useState } from 'react';
+import './scss/App.scss';
+import Box from './componet/Box';
+
+
+//1. 박스 2개 (타이틀, 사진, 결과)
+//2. 가위 바위 보 버튼
+//3. 버튼을 클릭하면 클릭한 값이 박스에 보임
+//4. 컴퓨터는 랜덤으로 설정
+//5. 3 4 의 결과를 가지고 누가 이겼는지 승패를 따진다
+//6. 승패결과에 따라 테두리 색이 바뀐다
+const choice = {
+  scissors:{
+    name:"Scissors",
+    img:"https://cdn-icons-png.flaticon.com/512/5418/5418004.png"
+  },
+  rock:{
+    name:"Rock",
+    img:"https://png.pngtree.com/png-clipart/20210228/ourmid/pngtree-beautiful-rock-in-the-river-png-png-image_2977572.jpg"
+  },
+  paper:{
+    name:"Paper",
+    img:"https://w7.pngwing.com/pngs/650/754/png-transparent-paper-clip-paper-torn.png"
+  }
+}
+
+function App() {
+  const [userSelect,setUserSelect] = useState(null);
+  const [computerSelect,setComputerSelect] = useState(null);
+  const [result,setResult] = useState("");
+
+  const play =(userChoice)=> {
+    setUserSelect(choice[userChoice]);
+
+    let computerChoice = reandomChoice()
+    setComputerSelect(computerChoice);
+
+    setResult(judgement(choice[userChoice],computerChoice));
+  };
+
+  const judgement =(user,computer)=> {
+    if(user.name == computer.name){
+      return "tie"
+    } else if(user.name == "Rock") 
+      return computer.name == "Scissors"?"win":"lose"
+    else if(user.name == "Scissors") 
+      return computer.name == "Paper"?"win":"lose"
+    else if(user.name == "Paper") 
+      return computer.name == "Rock"?"win":"lose"
+  };
+
+  const reandomChoice =()=> {
+    let itemArray = Object.keys(choice); //Object.keys 객체에 키값만 뽑아서 어레이로 만들어주는 함수
+    console.log("itemArray",itemArray);
+    let rendomItem = Math.floor(Math.random()*itemArray.length);
+    let final = itemArray[rendomItem]
+    return choice[final]
+  };
+
+  return (
+    <div>
+      <div className='main'>
+        <Box title="You" item={userSelect} result={result}/>
+        <Box title="Computer" item={computerSelect} result={result}/>
+      </div>
+      <div className='main'>
+        <button onClick={()=> play("scissors")}>가위</button>
+        <button onClick={()=> play("rock")}>바위</button>
+        <button onClick={()=> play("paper")}>보</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
+
